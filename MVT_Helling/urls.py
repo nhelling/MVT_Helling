69d155881816a37c8f176cc7c893aac9eb28e688
nhelling <nicolashelling@hotmail.com>
2023-01-07 03:02:31 -0300

    minuto 20 clase 20

--- MVT_Helling/urls.py
+++ MVT_Helling/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from MVT_Helling.views import inicio,programaciones, index
+from MVT_Helling.views import inicio, index
 
 
 urlpatterns = [
@@ -8,7 +8,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('Inicio/',inicio),
     
-    path('programaciones/', programaciones),
+    
     
     #path ('fleet/', include ('fleet.url')),
     path('flights/', include('flights.url')),
--- MVT_Helling/views.py
+++ MVT_Helling/views.py
@@ -9,6 +9,3 @@ def inicio(request):
     return HttpResponse("Bienvenidos a Planificación de Vuelos AEP") 
 
 
-#en vez de base va programacion o para probar base
-def programaciones(request):
-    return render(request, 'base.html', context={})
